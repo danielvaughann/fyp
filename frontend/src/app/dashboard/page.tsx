7d1db7f591c75ac294df089f944c10e489c5970e
Daniel <d.vaughan3@universityofgalway.ie>
2026-01-17 14:58:17 +0000

    test

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 export default function DashboardPage() {
     // user can be an object or none
+    //test
     const [user, setUser] = useState<{ id: number; email: string } | null>(null);
     const [error, setError] = useState("");
 
